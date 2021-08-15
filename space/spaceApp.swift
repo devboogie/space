3a98f9260cbfcf6ee3fab4ea2506f90a97c7a975
devboogie <goboogie@icloud.com>
2021-08-15 20:43:06 +0900

    Initial Commit

---
+++ space/spaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaceApp.swift
+//  space
+//
+//  Created by goboogie on 2021/08/15.
+//
+
+import SwiftUI
+
+@main
+struct spaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
